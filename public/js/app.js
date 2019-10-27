9ef677b160b11dc955bc0a6f24734bb99142a8d3
VladTGeorgiev <vlad.georgiev@outlook.com>
2019-10-27 21:09:31 +0000

    Added form and styling

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,28 @@
 console.log('Client side javascript file');
 
-fetch('http://localhost:3000/weather?address=!')
-    .then((res) => res.json())
-    .then((data) => {
-        if (data.error){
-            console.log(data.error)
-        }else{
-            console.log(data.location)
-            console.log(data.forecast)
-        }
+
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-one');
+const messageTwo = document.querySelector('#message-two');
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch(`http://localhost:3000/weather?address=${location}`)
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.error){
+                messageOne.textContent = data.error
+            }else{
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
     });
+});
